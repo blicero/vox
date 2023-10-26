80f6cf0f1438411b52fe8a57e61463d1a3cc8e50
Benjamin Walkenhorst <krylon@posteo.de>
2023-10-27 00:02:29 +0200

    Add assertions to constructors in module data

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2023-10-26 18:04:55 krylon>
+# Time-stamp: <2023-10-26 23:16:05 krylon>
 #
 # /data/code/python/vox/data.py
 # created on 25. 10. 2023
@@ -45,20 +45,31 @@ class File:  # pylint: disable-msg=R0902,R0903
 
     def __init__(self, **fields):
         if "file_id" in fields:
+            assert isinstance(fields["file_id"], int)
             self.file_id = fields["file_id"]
         if "program_id" in fields:
+            assert isinstance(fields["program_id"], int)
             self.program_id = fields["program_id"]
         if "path" in fields:
+            assert isinstance(fields["path"], str)
             self.path = fields["path"]
         if "title" in fields:
+            assert isinstance(fields["title"], str)
             self.title = fields["title"]
         if "disc_no" in fields:
+            assert isinstance(fields["disc_no"], int)
+            assert fields["disc_no"] > 0
             self.disc_no = fields["disc_no"]
         if "track_no" in fields:
+            assert isinstance(fields["track_no"], int)
+            assert fields["track_no"] > 0
             self.track_no = fields["track_no"]
         if "timestamp" in fields:
+            assert isinstance(fields["timestamp"], datetime)
             self.timestamp = fields["timestamp"]
         if "position" in fields:
+            assert isinstance(fields["position"], int)
+            assert fields["position"] >= 0
             self.position = fields["position"]
 
 
@@ -80,12 +91,16 @@ class Program:  # pylint: disable-msg=R0903
 
     def __init__(self, **fields):
         if "program_id" in fields:
+            assert isinstance(fields["program_id"], int)
             self.program_id = fields["program_id"]
         if "title" in fields:
+            assert isinstance(fields["title"], str)
             self.title = fields["title"]
         if "last_played" in fields:
+            assert isinstance(fields["last_played"], datetime)
             self.last_played = fields["last_played"]
         if "current_file" in fields:
+            assert isinstance(fields["current_file"], int)
             self.current_file = fields["current_file"]
 
 # Local Variables: #
