e30d68b7acfe1ff0a3905896af3337623c8a381a
Benjamin Walkenhorst <krylon@posteo.de>
2023-12-01 16:30:25 +0100

    Attempt to extract metadata for files, create Programs automatically

--- scanner.py
+++ scanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2023-11-06 20:44:19 krylon>
+# Time-stamp: <2023-12-01 16:28:38 krylon>
 #
 # /data/code/python/vox/scanner.py
 # created on 04. 11. 2023
@@ -22,13 +22,16 @@ import os
 import os.path
 import re
 from datetime import datetime
+from typing import Final
 
 import mutagen
 
 from vox import common, database
-from vox.data import File, Folder
+from vox.data import File, Folder, Program
 
-AUDIO_PAT: re.Pattern = re.compile("[.](?:mp3|og[ga]|opus|m4b|aac|flac)", re.I)
+AUDIO_PAT: Final[re.Pattern] = \
+    re.compile("[.](?:mp3|og[ga]|opus|m4b|aac|flac)", re.I)
+DISC_NO_PAT: Final[re.Pattern] = re.compile("(\\d+\\s*/\\s*(\\d+)")
 
 
 class Scanner:
@@ -70,12 +73,30 @@ class Scanner:
                                 path=full_path,
                             )
                             try:
-                                meta = mutagen.File(full_path)
+                                # meta = mutagen.File(full_path)
+                                # # album: str = ""
 
-                                if "title" in meta:
-                                    db_file.title = meta["title"][0]
-                                if "track" in meta:
-                                    db_file.ord2 = int(meta["track"][0])
+                                # if "title" in meta:
+                                #     db_file.title = meta["title"][0]
+                                # if "track" in meta:
+                                #     db_file.ord2 = int(meta["track"][0])
+                                meta = read_tags(full_path)
+                                if meta["album"] == "":
+                                    db_file.program_id = 0
+                                else:
+                                    prog = self.db.program_get_by_title(meta["album"])  # noqa: E501
+                                    if prog is None:
+                                        prog = Program(
+                                            title=meta["album"],
+                                            creator=meta["artist"],
+                                        )
+                                        self.db.program_add(prog)
+                                        db_file.program_id = prog.program_id
+
+                                db_file.ord1 = int(meta["ord1"])
+                                db_file.ord2 = int(meta["ord2"])
+                                db_file.title = meta["title"]
+                                self.db.file_add(db_file)
                             except Exception as e:  # pylint: disable-msg=W0718
                                 self.log.error("Caught exception while handling metadata: %s", e)  # noqa: E501 # pylint: disable-msg=C0301
                             self.db.file_add(db_file)
@@ -90,6 +111,49 @@ class Scanner:
         folders = self.db.folder_get_all()
         for f in folders:
             self.scan(f.path)
+
+
+def read_tags(path: str) -> dict[str, str]:
+    """Attempt to extract metadata from an audio file.
+
+    path is expected to be the full, absolute path.
+    """
+    meta = mutagen.File(path)
+    tags: dict[str, str] = {
+        "artist": "",
+        "album": "",
+        "title": "",
+        "ord1": "0",
+        "ord2": "0",
+    }
+
+    if "artist" in meta:
+        tags["artist"] = meta["artist"]
+    elif "TPE1" in meta:
+        tags["artist"] = meta["TPE1"].text[0]
+
+    if "album" in meta:
+        tags["album"] = meta["album"]
+    elif "TALB" in meta:
+        tags["album"] = meta["TALB"].text[0]
+    else:
+        tags["album"] = os.path.basename(os.path.dirname(path))
+
+    if "title" in meta:
+        tags["title"] = meta["title"]
+    elif "TIT2" in meta:
+        tags["title"] = meta["TIT2"].text[0]
+
+    if "tracknumber" in meta:
+        tags["ord2"] = meta["tracknumber"]
+    elif "TRCK" in meta:
+        tags["ord2"] = meta["TRCK"].text[0]
+
+    if "discnumber" in meta:
+        tags["ord1"] = meta["discnumber"]
+
+    return tags
+
 
 # Local Variables: #
 # python-indent: 4 #
