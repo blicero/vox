00ce576d32cb07d5606a6664d339b9a247e4d977
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-06 18:33:58 +0200

    Add module network

---
+++ network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2024-08-06 18:33:45 krylon>
+#
+# /data/code/python/vox/network.py
+# created on 05. 08. 2024
+# (c) 2024 Benjamin Walkenhorst
+#
+# This file is part of the Vox audiobook reader. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+vox.network
+
+(c) 2024 Benjamin Walkenhorst
+"""
+
+
+class Network:
+    """Network provides discovery and synchronization across multiple devices."""
+
+    __slots__ = [
+        "hostname",
+    ]
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
