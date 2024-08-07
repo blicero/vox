2ad8ef2fb7ec45e7f2eee61c7b4dc62129fdfd99
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-07 18:29:35 +0200

    Start working on service discovery

--- network.py
+++ network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2024-08-06 18:33:45 krylon>
+# Time-stamp: <2024-08-07 18:28:22 krylon>
 #
 # /data/code/python/vox/network.py
 # created on 05. 08. 2024
@@ -16,13 +16,52 @@ vox.network
 (c) 2024 Benjamin Walkenhorst
 """
 
+from socket import gethostname
+from typing import Final
+
+from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
+
+DefaultPort: Final[int] = 5281
+
+
+class VoxListener(ServiceListener):
+    """Represent."""
+
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} updated")
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} removed")
+
+    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        info = zc.get_service_info(type_, name)
+        print(f"Service {name} added, service info: {info}")
+
 
 class Network:
     """Network provides discovery and synchronization across multiple devices."""
 
     __slots__ = [
         "hostname",
+        "mdns",
     ]
+
+    hostname: str
+    mdns: Zeroconf
+
+    def __init__(self) -> None:
+        self.hostname = gethostname()
+        self.mdns = Zeroconf()
+
+
+if __name__ == '__main__':
+    zc = Zeroconf()
+    listener = VoxListener()
+    browser = ServiceBrowser(zc, "_http._tcp.local.", listener)
+    try:
+        input("Press enter to quit.\n\n")
+    finally:
+        zc.close()
 
 # Local Variables: #
 # python-indent: 4 #
